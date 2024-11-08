bcdc3144d0fd35bbf45adc23efc3eeda3ab32237
Semil P <87702420+semil007@users.noreply.github.com>
2024-11-08 13:50:34 +0530

    Delete embedding_handler.py

--- embedding_handler.py
+++
@@ -1,4 +0,0 @@
-from langchain.embeddings import HuggingFaceEmbeddings
-
-def get_huggingface_embeddings(model_name='sentence-transformers/all-MiniLM-L6-v2'):
-    return HuggingFaceEmbeddings(model_name=model_name)
